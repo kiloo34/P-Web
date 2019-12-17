57de9c90271ced13b85b0aa6826b52a16aa9868e
kiloo34 <uchiharobi@rocketmail.com>
2019-12-17 19:34:09 +0700

    fitur ajax eror nyerah aing,  extend jquery fix

--- asset/js/validasi.js
+++ asset/js/validasi.js
@@ -1,3 +1,88 @@
+function modalShow() {
+    $("#id_modal").show();
+    $("#id_modal").css('animation-name', 'show_modal');
+    // $(".modal-content:first").css('animation-name', 'show_modal');
+}
+
+function modalHide() {
+    // $(".modal-content:first").css('animation-name', 'hide_modal');
+    $("#id_modal").css('animation-name', 'hide_modal');
+    setTimeout(function function_name() {
+        $("#id_modal").hide();
+    }, 300);
+}
+
+function show_error(pesan) {
+    $("#error_modal").find('b').html(pesan);
+    $("#error_modal").show();
+    $("#error_modal").css('animation-name', 'show_modal');
+
+    $("#close_error_form").click(function (event) {
+        // $("#error_modal").hide();
+        $("#error_modal").css('animation-name', 'hide_modal');
+        setTimeout(function function_name() {
+            $("#error_modal").hide();
+        }, 300);
+    });
+
+    window.onclick = function (event) {
+        var modal = document.getElementById('error_modal');
+        if (event.target == modal) {
+            $("#error_modal").css('animation-name', 'hide_modal');
+            setTimeout(function function_name() {
+                $("#error_modal").hide();
+            }, 300);
+        }
+    };
+}
+
+$.fn.validateEmpty = function () {
+    var form = this;
+
+    form.on('submit', function (event) {
+        event.preventDefault();
+        var inputan = form.find("input,select");
+        // console.log(inputan);
+        var kosong = false;
+        inputan.each(function (index, el) {
+            if ($(el).val() == "") {
+                kosong = true;
+            }
+        });
+
+        if (kosong) {
+            show_error("Harap lengkapi data isian");
+        } else {
+            form.off('submit').trigger('submit');
+        }
+    });
+}
+
+$(function () {
+    var imagesPreview = function (input, placeToInsertImagePreview) {
+
+        if (input.files) {
+            var filesAmount = input.files.length;
+
+            for (i = 0; i < filesAmount; i++) {
+                var reader = new FileReader();
+
+                reader.onload = function (event) {
+                    $($.parseHTML('<img>')).attr('src', event.target.result).appendTo(
+                        placeToInsertImagePreview);
+                }
+
+                reader.readAsDataURL(input.files[i]);
+            }
+        }
+
+    };
+
+    $('#gallery-photo-add').on('change', function () {
+        imagesPreview(this, 'div.gallery');
+    });
+});
+
 function validateForm() {
     var x = document.forms["form"]["username"].value;
     var y = document.forms["form"]["password"].value;
@@ -24,6 +109,7 @@ function validateForm() {
 }
 
 function validateFormProduk() {
+
     var x = document.forms["form"]["nama"].value;
     var y = document.forms["form"]["harga"].value;
     var a = document.forms["form"]["deskripsi"].value;
